54de492dcc5b31a1fabbcb09b67257849fa812eb
Van <wanniejvr08@gmail.com>
2024-02-10 17:58:56 +0200

    UX(Column): basic styling

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -10,8 +10,38 @@ function ColumnContainer(props: Props) {
     <div className="
     bg-columnBackgroundColour
     text-white
+    w-[350px]
+    h-[500px]
+    max-h-[500px]
+    rounded-md
+    flex
+    flex-col
     "
-    >{column.title}</div>
+    >
+      {/* Task title */}
+      <div className="
+      bg-mainBackgroundColour
+      text-md
+      h-[60px]
+      cursor-grab
+      rounded-md
+      rounded-b-none
+      p-3
+      font-bold
+      border-columnBackgroundColour
+      border-4
+      ">
+        {column.title}
+      </div>
+      {/* Task body */}
+      <div className="flex flex-grow">
+        Content
+      </div>
+      {/* Task footer */}
+      <div>
+        footer
+      </div>
+    </div>
   )
 }
 
